365a54e9d6d0cb8ffea069bb6517831e02f00c96
darshan-trivedi-10 <dbtrivedi03@gmail.com>
2022-09-24 12:24:26 +0530

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import cors from 'cors'
+import cors from 'cors'  
 import dotenv from 'dotenv';
 
 // Routers
