d90cc105fedcfbfc53f34ac1a5669f56df0e1ec3
darshan-trivedi-10 <dbtrivedi03@gmail.com>
2022-09-24 23:01:25 +0530

    change in code

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const corsOptions = {
     optionSuccessStatus: 200
 }
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
