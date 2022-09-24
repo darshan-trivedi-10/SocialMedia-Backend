b5d2db1a36c34bde5bb0315c4289d948b0573eba
darshan-trivedi-10 <dbtrivedi03@gmail.com>
2022-09-24 22:31:33 +0530

    change in code

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -31,6 +31,11 @@ export const createUser = async (req, res) => {
 
 // login
 export const LoginUser = async (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
     const { username, password } = req.body;
     try {
         const user = await userModel.findOne({ username: username });
