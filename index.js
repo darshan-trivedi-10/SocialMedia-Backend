04f1ac03f15bbb8fed4e8ea37e0f1ae027f276f7
darshan-trivedi-10 <dbtrivedi03@gmail.com>
2022-09-25 15:38:03 +0530

    change in code

--- index.js
+++ index.js
@@ -38,10 +38,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Headers", "*")
     // "Origin, X-Requested, Content-Type, Accept Authorization"
-    res.header(
-        "Access-Control-Allow-Methods",
-        "POST, PUT, PATCH, GET, DELETE")
-    return res.status(200).json({})
+    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE")
 
     next()
 })
