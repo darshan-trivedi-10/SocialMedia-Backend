cd313f310fa019f1d0ea3106c624821f416fce02
darshan-trivedi-10 <dbtrivedi03@gmail.com>
2022-09-25 11:08:12 +0530

    change in code

--- Routes/AuthRoute.js
+++ Routes/AuthRoute.js
@@ -1,10 +1,9 @@
 import express from "express";
-import cors from 'cors'
 import { createUser, LoginUser } from '../Controllers/AuthController.js'
 const AuthRoute = express.Router();
 
 
-AuthRoute.post('/register', cors(), createUser) 
-AuthRoute.post('/login', cors(), LoginUser)
+AuthRoute.post('/register', createUser)
+AuthRoute.post('/login', LoginUser)
 
 export default AuthRoute;
