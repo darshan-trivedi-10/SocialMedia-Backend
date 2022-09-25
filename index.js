ed157a38a93e6a5faf84649337f58b89e0a27fd1
darshan-trivedi-10 <dbtrivedi03@gmail.com>
2022-09-25 15:17:31 +0530

    change in code

--- index.js
+++ index.js
@@ -36,15 +36,13 @@ dotenv.config();
 app.use(cors());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers","*")
+    res.header("Access-Control-Allow-Headers", "*")
     // "Origin, X-Requested, Content-Type, Accept Authorization"
-    if (req.method === "OPTIONS") {
-        res.header(
-            "Access-Control-Allow-Methods",
-            "POST, PUT, PATCH, GET, DELETE"
-        )
-        return res.status(200).json({})
-    }
+    res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE")
+    return res.status(200).json({})
+
     next()
 })
 mongoose.connect('mongodb+srv://darshan:tDj0mhMWHdRkggKR@cluster0.knu7pzn.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
